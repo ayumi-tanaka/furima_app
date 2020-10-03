6fd182a3d41a90810f8abd7dd8b69a5b2248b413
ayumi-tanaka <67615448+ayumi-tanaka@users.noreply.github.com>
2020-10-03 16:37:19 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'items#index'
-=======
   devise_for :users
   root to: 'items#index'
->>>>>>> Stashed changes
   resources :items
 end
